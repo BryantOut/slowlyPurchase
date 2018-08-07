4dba8e9d7dade2c88c29d391343ad966d9fc0072
waiter-yu <18720528747@163.com>
2018-08-07 21:08:32 +0800

    yurenping

--- js/youhui.js
+++ js/youhui.js
@@ -19,18 +19,20 @@ $.ajax({
         var htmls = template('goods', { data: res.result });
         $('#tips').html(htmls);
         //总页数
-        num = Math.floor(res.totalCount / 10);
+        num = Math.ceil(res.totalCount / 10);
         var strhtml = "";
         for (i = 0; i < num; i++) {
-            strhtml += '<option value="' + (i + 1) + '">' + (i + 1) + '/' + num + '</option>';
+            strhtml += '<option value="' + i+ '">' + (i + 1) + '/' + num + '</option>';
         }
         $('#selectid').html(strhtml);
         //上一页点击事件
         $('.prey').on("touchstart", function () {
             index = $('#selectid').prop('selected', true).val();
+            console.log(index);
+            
             index--;
             console.log(index);
-            if (index == 1) {
+            if (index == -1) {
                 layer.open({
                     content: '已经是第一页了'
                     ,skin: 'msg'
@@ -66,7 +68,7 @@ $.ajax({
             index = $('#selectid').prop('selected', true).val();
             index++;
             console.log(num);
-            if (index > num) {
+            if (index > num-1) {
                 layer.open({
                     content: '已经是最后一页了'
                     ,skin: 'msg'
@@ -98,97 +100,6 @@ $.ajax({
 
     }
 })
-// $.get("http://193.112.55.79:9090/api/getmoneyctrl", { "pageid": index }, function (res) {
-//     var htmls = template('goods', { data: res.result });
-//     $('#tips').html(htmls);
-// }, "json")
-// $('#selectid').val(index);
-
-//下一页
-// $(".next").on("touchstart", function () {
-//     var index = $('#selectid').prop('selected', true).val();
-//     index++;
-//     console.log(num);
-
-//     if (index > num) {
-//         alert('已经是最后一个了');
-//         return;
-//         // console.log(12);
-//     }
-//     $.get("http://193.112.55.79:9090/api/getmoneyctrl", { "pageid": index }, function (res) {
-//         var htmls = template('goods', { data: res.result });
-//         $('#tips').html(htmls);
-//         $('#selectid').val(index);
-//         num = Math.floor(res.totalCount / 10);
-//     }, "json");
-// });
-
-
-// console.log(1)
-// $.get("http://193.112.55.79:9090/api/getmoneyctrl",
-//     "", function (res) {
-//         console.log(res);
-//         console.log(1);
-//         var htmls = template('goods', { data: res.result });
-//         // console.log(htmls);
-//         $('#tips').html(htmls);
-//         num = Math.floor(res.totalCount / 10);
-//         var strhtml = "";
-//         for (i = 0; i < num; i++) {
-
-//             strhtml += '<option value="' + (i + 1) + '">' + (i + 1) + '/' + num + '</option>';
-//         }
-//         $('#selectid').html(strhtml);
-
-//         $('.prey').on("touchstart", function () {
-//             var index = $('#selectid').prop('selected', true).val();
-//             if (index == 1) {
-//                 alert('第一页');
-//                 return;
-//             }
-//             index--;
-//             $.get("http://193.112.55.79:9090/api/getmoneyctrl", { "pageid": index }, function (res) {
-//                 var htmls = template('goods', { data: res.result });
-//                 $('#tips').html(htmls);
-//             }, "json")
-//             $('#selectid').val(index);
-//         })
-//         //下一页
-//         $(".next").on("touchstart", function () {
-//             var index = $('#selectid').prop('selected', true).val();
-//             index++;
-//             console.log(num);
-
-//             if (index > num) {
-//                 alert('已经是最后一个了');
-//                 return;
-//                 // console.log(12);
-//             }
-//             $.get("http://193.112.55.79:9090/api/getmoneyctrl", { "pageid": index }, function (res) {
-//                 var htmls = template('goods', { data: res.result });
-//                 $('#tips').html(htmls);
-//                 $('#selectid').val(index);
-//                 num = Math.floor(res.totalCount / 10);
-//             }, "json");
-//         });
-
-//         console.log(num)
-//     }, "json");
-//上一页
-
-
-
-//选择页码
-// $.ajax({
-//     url:'http://193.112.55.79:9090/api/getmoneyctrl',
-//     type:'get',
-//     data:{"pageid": pageid },
-//     dataType:'json',
-//     success:function(res){
-//         var htmls = template('goods', { data: res.result });
-//         $('#tips').html(htmls);
-//     }
-// })
 $("#selectid").on('change', function () {
     var pageid = $(this).val();
     // $.get("http://193.112.55.79:9090/api/getmoneyctrl", { "pageid": pageid }, function (res) {
